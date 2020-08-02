fa3a5c259da43e9f63b2a9cf78848ff88d5f3eb8
Jehanne Hodge <samarahjhodge@gmail.com>
2020-08-02 12:35:51 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,93 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+# user1 = User.create([
+#   {
+#     name: "Paolo Maldini",
+#     email: "paolomaldini@maldini.com",
+#     password_digest: "1234"
+#   }
+# ])
+
+10.times do
+  User.create(name: Faker::Name.name, email: Faker::Internet.email, password_digest: "1234")
+end
+  
+ recyclings = Recycling.create([
+  {
+    name: "Glass"
+  },
+  {
+    name: "Plastic"
+  },
+  {
+    name: "Aluminum"
+  },
+  {
+    name: "Plastic Bottle"
+  },
+  {
+    name: "Plastic Fork"
+  },
+  {
+    name: "Soda Can"
+  },
+  {
+    name: "Book"
+  },
+  {
+    name: "Paper"
+  },
+  {
+    name: "Mason Jar"
+  },
+  {
+    name: "Paper Envelope"
+  }
+])
+
+3.times do
+  Organic.create(name: Faker::Food.fruits)
+end
+
+4.times do
+  Organic.create(name: Faker::Food.vegetables)
+end
+
+3.times do
+  Organic.create(name: Faker::Dessert.variety)
+end
+
+garbages = Garbage.create([
+  {
+    name: "Fabric"
+  },
+  {
+    name: "Black Plastic Container"
+  },
+  {
+    name: "Hot Drink Cup"
+  },
+  {
+    name: "Wrapping Paper"
+  },
+  {
+    name: "Styrofoam"
+  },
+  {
+    name: "Cigarette Butt"
+  },
+  {
+    name: "Light Bulb"
+  },
+  {
+    name: "Gum Wrapper"
+  },
+  {
+    name: "Straw"
+  },
+  {
+    name: "Milk Box"
+  }
+])
